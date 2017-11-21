9eb61db85d5c0f44bf518b5f0f4de26ec7f9c834
joshS28 <staffojo@tcd.ie>
2017-11-21 20:20:35 +0000

    Updated JS to allow user creation in Firebase

--- index.js
+++ index.js
@@ -16,33 +16,27 @@ firebase.auth().onAuthStateChanged(function(user) {
      var user = firebase.auth().currentUser;
      var name, email, photoUrl, uid, emailVerified;
      email = user.email;
-     name = user.displayName;
+     //name = user.displayName;
      uid = user.uid;
-    
-     var users = database.child("users");
-     //var newEmail = email.replace(".", ",");
-     //alert(newEmail);
-     //users.child("email").set({newEmail : {week1: 10, week2: 11} });
 
-     
+     var user = firebase.auth().currentUser;
+        var name, email, photoUrl, uid, emailVerified;
+        //email = user.email;
+    // name = user.displayName;
+     //uid = user.uid;
 
-    
- 
+       function escapeEmailAddress(email) {
+        if (!email) return false
+        // Replace '.' (not allowed in a Firebase key) with ',' (not allowed in an email address)
+        console.log("inside loop - working");
+        email = email.toLowerCase();
+        email = email.replace(/\./g, ',');
+        console.log("finished loop - working");
+        return email;
+        }
 
-    if(name == null){
-      var x = document.getElementById('noName');
-      x.style.display = "block";
-      var nameEntry = document.getElementById('nameInput').value;
-      user.updateProfile({
-        displayName: nameEntry
-      }).then(function(){
-        document.getElementById('nameBody').innerHTML = "Hey " + email + " we found your name, it's " + nameEntry;
-      });
-    }
-    else{
-      document.getElementById('nameBody').innerHTML = "Hey " + email + " we found your name, it's " + name;
+        
 
-    }   
      document.getElementById('checkName').innerHTML = "Hey " + uid + " !";
 
 
@@ -90,7 +84,7 @@ $("#loginBtn").click(
 
 $("#signupBtn").click(
   function(){
-
+    console.log("sign up btn clicked");
 
     var email = $("#loginEmail").val();
     var password = $("#loginPassword").val();
@@ -100,34 +94,45 @@ $("#signupBtn").click(
       $("#loginBtn").hide();
 
       firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
+
+        console.log("New user created");
+
         // Handle Errors here.
         var errorCode = error.code;
         var errorMessage = error.message;
         var database = firebase.database().ref();
+        
     
-     var user = firebase.auth().currentUser;
-     var name, email, photoUrl, uid, emailVerified;
-     email = user.email;
-     name = user.displayName;
-     uid = user.uid;
-     var users = database.child("users");
-
-     function escapeEmailAddress(email) {
-      if (!email) return false
-      // Replace '.' (not allowed in a Firebase key) with ',' (not allowed in an email address)
-      email = email.toLowerCase();
-      email = email.replace(/\./g, ',');
-      return email;
-      }
+        
       
-      var myUser = users.child(escapeEmailAddress(user.email));
-      myUser.set({ email: user.email, name: user.displayName, phone: 12912912 });
 
 
         $("#loginError").show().text(errorMessage);
         $("#loginProgress").hide();
         $("#loginBtn").show();
       });
+      var database = firebase.database().ref();
+        console.log("Reference made");
+        var users = database.child("users");
+        var user = firebase.auth().currentUser;
+        //var displayName = user.displayName;
+        function escapeEmailAddress(email) {
+        if (!email) return false
+        // Replace '.' (not allowed in a Firebase key) with ',' (not allowed in an email address)
+        console.log("inside loop - working");
+        email = email.toLowerCase();
+        email = email.replace(/\./g, ',');
+        console.log("finished loop - working");
+        return email;
+        }
+        console.log("calling loop - working");
+        var myUser = users.child(escapeEmailAddress(email));
+        console.log("setting variables - working");
+        myUser.set({ email: email});
+
+        console.log("variables set - working");
+
+
     }
   }
 );

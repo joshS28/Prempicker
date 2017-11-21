8d0f3e771a45d7a4e208c59f3c28794b4304aa03
joshS28 <staffojo@tcd.ie>
2017-11-21 01:54:46 +0000

    New user sign up populates DB

--- index.js
+++ index.js
@@ -11,12 +11,23 @@ firebase.auth().onAuthStateChanged(function(user) {
     */
     dialog.close();
 
-    var user = firebase.auth().currentUser;
-    var name, email, photoUrl, uid, emailVerified;
-    email = user.email;
-    name = user.displayName;
+     var database = firebase.database().ref();
     
-    document.getElementById('nameBody').innerHTML = "Hey " + email;
+     var user = firebase.auth().currentUser;
+     var name, email, photoUrl, uid, emailVerified;
+     email = user.email;
+     name = user.displayName;
+     uid = user.uid;
+    
+     var users = database.child("users");
+     //var newEmail = email.replace(".", ",");
+     //alert(newEmail);
+     //users.child("email").set({newEmail : {week1: 10, week2: 11} });
+
+     
+
+    
+ 
 
     if(name == null){
       var x = document.getElementById('noName');
@@ -31,9 +42,8 @@ firebase.auth().onAuthStateChanged(function(user) {
     else{
       document.getElementById('nameBody').innerHTML = "Hey " + email + " we found your name, it's " + name;
 
-    }
-     document.getElementById('checkName').innerHTML = "Hey " + user.displayName + " !";
-
+    }   
+     document.getElementById('checkName').innerHTML = "Hey " + uid + " !";
 
 
   } else {
@@ -93,6 +103,26 @@ $("#signupBtn").click(
         // Handle Errors here.
         var errorCode = error.code;
         var errorMessage = error.message;
+        var database = firebase.database().ref();
+    
+     var user = firebase.auth().currentUser;
+     var name, email, photoUrl, uid, emailVerified;
+     email = user.email;
+     name = user.displayName;
+     uid = user.uid;
+     var users = database.child("users");
+
+     function escapeEmailAddress(email) {
+      if (!email) return false
+      // Replace '.' (not allowed in a Firebase key) with ',' (not allowed in an email address)
+      email = email.toLowerCase();
+      email = email.replace(/\./g, ',');
+      return email;
+      }
+      
+      var myUser = users.child(escapeEmailAddress(user.email));
+      myUser.set({ email: user.email, name: user.displayName, phone: 12912912 });
+
 
         $("#loginError").show().text(errorMessage);
         $("#loginProgress").hide();
